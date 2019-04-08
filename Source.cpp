79fde2ce2faa14dbe86db60006ee47ebd16123c5
123andrey-petrenko321 <48992351+123andrey-petrenko321@users.noreply.github.com>
2019-04-08 16:14:10 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,6 @@
 
+/*
+
 #include <iostream>
 #include <math.h>
 using namespace std;
@@ -28,6 +30,37 @@ int main()
 	}
 	system("pause");
 	return 0;
-}
+}*/
 
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	const int row = 5;
+	const int col = 5;
+	int i, j;
+	int mas[row][col];
+
+	for (i = 0 ; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			mas[i][j] = (i + 1) * 10 + (j + 1);
+		}
+	}
+
+	for (i = 0 ; i < j  ; i ++)
+	{
+		for (j = 0;  j + i  < col ; j++)
+		{
+			cout << mas[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
 
